fe5f2a2083b935317ed836507ffb04bbac78706f
Olatunnji TOlulope <olatunjitolulope4@gmail.com>
2022-07-21 12:30:21 +0100

    component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.scss";
+import "@progress/kendo-theme-material/dist/all.css";
+import "hammerjs";
+import Navigation from "./Components/Navigation";
+import Onboarding from "./Components/Onboarding";
+import Hero from "./Components/Hero";
+import Market from "./Components/Market";
+import Message from "./Components/Message";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Onboarding />
+      <Hero/>
+      <Market />
+      <Message />
+      <Footer />
     </div>
   );
 }
